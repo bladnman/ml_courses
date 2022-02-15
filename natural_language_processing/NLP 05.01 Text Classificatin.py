be3309b346b7e3a20ab926b1c64329eebb571625
matt@maher-family.net <matt@maher-family.net>
2022-02-15 01:13:14 +0000

    getting things set up

---
+++ natural_language_processing/NLP 05.01 Text Classificatin.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### Setup
+
+# COMMAND ----------
+
+import sys
+sys.path.append("/Workspace/Repos/bladnman@gmail.com/databricks_tools")
+from brick_utils.SparkUtils_01 import SparkUtils
+su = SparkUtils(spark)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Coursework
+
+# COMMAND ----------
+
+
